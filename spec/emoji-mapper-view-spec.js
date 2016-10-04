f5b50aadac951dbe2f83be4bd6d816cc32073d17
Nikola Pejoski <nikola.pejoski@klarna.com>
2016-10-04 21:29:30 +0200

    Rename and cleanup 💨

--- spec/emoji-mapper-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import EmojiMapperView from '../lib/mog-script-snippets-view';
-
-describe('EmojiMapperView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
