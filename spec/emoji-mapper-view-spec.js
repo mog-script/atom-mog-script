c79228fea54e38db83353b03e5f55e3bd0819502
Nikola Pejoski <nikola.pejoski@klarna.com>
2016-10-04 21:15:27 +0200

    Initial version of the atom plugin for M.O.G script

---
+++ spec/emoji-mapper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmojiMapperView from '../lib/mog-script-snippets-view';
+
+describe('EmojiMapperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
